e4efbdc91c1168923f3a3ee2eb4b2e2e01421208
Keith Lee <keithlee@Keithde-MacBook-Pro.local>
2017-06-28 19:13:31 +0800

    string able to add format

--- String+Localization.swift
+++ String+Localization.swift
@@ -12,3 +12,19 @@ extension String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
     }
 }
+
+class StringResourceUtility {
+    static func Localizer(tableName: String?) -> (_ key: String, _ params: CVaListPointer) -> String {
+        return { (key: String, params: CVaListPointer) in
+            let content = NSLocalizedString(key, tableName: tableName, comment: "")
+            return NSString(format: content, arguments: params) as String
+        }
+    }
+}
+
+extension String {
+    func format(with params: CVarArg...) -> String {
+        let t = StringResourceUtility.Localizer(tableName: nil)
+        return withVaList(params) { t(self, $0) }
+    }
+}
