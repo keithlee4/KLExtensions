db379bbdc4b6091337c3f08e709d25704ba2d178
Keith <keith.lee@git4u.com>
2018-01-23 15:05:11 +0800

    Fix order min amt issues

--- Formatter+QuickGen.swift
+++ Formatter+QuickGen.swift
@@ -17,8 +17,8 @@ extension Decimal {
         return dec.doubleValue.rounded(toPlaces: 8, rule: FloatingPointRoundingRule.toNearestOrEven)
     }
     
-    func asString(digits: Int, force: Bool = false, separator sep: String = "") -> String {
-        return NumberFormatter.decimalString(fromDecimal: self, withDigits: digits, separator: sep, forceDigit: force)!
+    func asString(digits: Int, force: Bool = false, separator sep: String = "", roundMode:  NumberFormatter.RoundingMode = .floor) -> String {
+        return NumberFormatter.decimalString(fromDecimal: self, withDigits: digits, separator: sep, forceDigit: force, roundMode: roundMode)!
     }
 }
 
@@ -27,14 +27,14 @@ extension Double {
         return Decimal.init(self.rounded(toPlaces: 8))
     }
     
-    func asString(digits: Int, force: Bool = false, separator sep: String = "") -> String {
-        return NumberFormatter.decimalString(fromDoubleValue: self, withDigits: digits, separator: sep, forceDigit: force)!
+    func asString(digits: Int, force: Bool = false, separator sep: String = "", roundMode:  NumberFormatter.RoundingMode = .floor) -> String {
+        return NumberFormatter.decimalString(fromDoubleValue: self, withDigits: digits, separator: sep, forceDigit: force, roundMode: roundMode)!
     }
 }
 
 extension Float {
-    func asString(digits: Int, force: Bool = false, separator sep: String = "") -> String {
-        return NumberFormatter.decimalString(fromFloatValue: self, withDigits: digits, separator: sep, forceDigit: force)!
+    func asString(digits: Int, force: Bool = false, separator sep: String = "", roundMode:  NumberFormatter.RoundingMode = .floor) -> String {
+        return NumberFormatter.decimalString(fromFloatValue: self, withDigits: digits, separator: sep, forceDigit: force, roundMode: roundMode)!
     }
 }
 
@@ -69,7 +69,7 @@ extension DateFormatter {
 }
 
 extension NumberFormatter {
-    static func decimalString(fromDecimal: Decimal, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false) -> String? {
+    static func decimalString(fromDecimal: Decimal, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false, roundMode: NumberFormatter.RoundingMode) -> String? {
         
         let ns = NumberFormatter.init()
         ns.allowsFloats = true
@@ -79,15 +79,31 @@ extension NumberFormatter {
         }
         
         
-        ns.roundingMode = .floor
+        ns.roundingMode = roundMode
         ns.numberStyle = .decimal
         ns.groupingSeparator = sep
+        var rule: FloatingPointRoundingRule
+        switch roundMode {
+        case .ceiling:
+            rule = .awayFromZero
+        case .down:
+            rule = .down
+        case .up:
+            rule = .up
+        case .floor:
+            rule = .towardZero
+        case .halfDown, .halfUp, .halfEven:
+            rule = .toNearestOrEven
+        }
         
-        return ns.string(from: NSNumber.init(value: fromDecimal.doubleValue))
+        let dec = NSDecimalNumber.init(decimal: fromDecimal).doubleValue.rounded(toPlaces: digits, rule: rule).decimalValue
+        let dValue = dec.doubleValue
+        let str = ns.string(from: NSNumber.init(value: dValue))
+        return str
     }
     
     
-    static func decimalString(fromFloatValue:Float, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false) -> String? {
+    static func decimalString(fromFloatValue:Float, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false, roundMode: NumberFormatter.RoundingMode) -> String? {
         let ns = NumberFormatter.init()
         ns.allowsFloats = true
         ns.maximumFractionDigits = digits
@@ -96,21 +112,21 @@ extension NumberFormatter {
         }
         
         
-        ns.roundingMode = .floor
+        ns.roundingMode = roundMode
         ns.numberStyle = .decimal
         ns.groupingSeparator = sep
         
         return ns.string(from: NSNumber.init(value: fromFloatValue))
     }
     
-    static func decimalString(fromDoubleValue:Double, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false) -> String? {
+    static func decimalString(fromDoubleValue:Double, withDigits digits:Int, separator sep: String = "", forceDigit: Bool = false, roundMode: NumberFormatter.RoundingMode) -> String? {
         let ns = NumberFormatter.init()
         ns.allowsFloats = true
         ns.maximumFractionDigits = digits
         if forceDigit {
             ns.minimumFractionDigits = digits
         }
-        ns.roundingMode = .floor
+        ns.roundingMode = roundMode
         ns.numberStyle = .decimal
         ns.groupingSeparator = sep
         
@@ -118,30 +134,30 @@ extension NumberFormatter {
     }
 
     
-    static func decimalString(from number:NSNumber, withDigits digits:Int) -> String? {
+    static func decimalString(from number:NSNumber, withDigits digits:Int, roundMode: NumberFormatter.RoundingMode) -> String? {
         let ns = NumberFormatter.init()
         ns.allowsFloats = true
         ns.maximumFractionDigits = digits
         ns.numberStyle = .decimal
-        ns.roundingMode = .floor
+        ns.roundingMode = roundMode
         
         return ns.string(from: number)
     }
     
-    static func decimalNoDigitString(from number:NSNumber) -> String? {
-        return self.decimalString(from: number, withDigits: 0)
+    static func decimalNoDigitString(from number:NSNumber, roundMode: NumberFormatter.RoundingMode) -> String? {
+        return self.decimalString(from: number, withDigits: 0, roundMode: roundMode)
     }
     
-    static func numberOfDecimalString(from string:String, withDigits digits:Int) -> NSNumber? {
+    static func numberOfDecimalString(from string:String, withDigits digits:Int, roundMode: NumberFormatter.RoundingMode) -> NSNumber? {
         let ns = NumberFormatter.init()
         ns.allowsFloats = true
         ns.maximumFractionDigits = digits
         ns.numberStyle = .decimal
-        ns.roundingMode = .floor
+        ns.roundingMode = roundMode
         return ns.number(from: string)
     }
     
-    static func numberOfNoDigitsDecimalString(from string:String) -> NSNumber? {
-        return self.numberOfDecimalString(from: string, withDigits: 0)
+    static func numberOfNoDigitsDecimalString(from string:String, roundMode:  NumberFormatter.RoundingMode = .floor) -> NSNumber? {
+        return self.numberOfDecimalString(from: string, withDigits: 0, roundMode: roundMode)
     }
 }
